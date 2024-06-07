83d77f4541266c8a6da97ed09a8845f68a0664ae
Sajeyks <sajeyksmwangi@gmail.com>
2024-06-07 16:31:00 +0300

    Refactor user and post models for improved readability and documentation

--- app/models/post.py
+++ app/models/post.py
@@ -1,9 +1,17 @@
-# post.py
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from app.database import Base
 
 class Post(Base):
+    """
+    Represents a post in the database.
+
+    Attributes:
+        id (int): The unique identifier for the post.
+        text (str): The content of the post.
+        owner_id (int): The ID of the user who created the post.
+        owner (relationship): Relationship with the User model indicating the owner of the post.
+    """
     __tablename__ = "posts"
     id = Column(Integer, primary_key=True, index=True)
     text = Column(String(255), index=True)
--- app/models/user.py
+++ app/models/user.py
@@ -1,10 +1,18 @@
-# user.py
 from sqlalchemy import Column, Integer, String
 from app.database import Base
 from sqlalchemy.orm import relationship
 from .post import Post
 
 class User(Base):
+    """
+    Represents a user in the database.
+
+    Attributes:
+        id (int): The unique identifier for the user.
+        email (str): The email address of the user (unique).
+        hashed_password (str): The hashed password of the user.
+        posts (relationship): Relationship with the Post model indicating posts created by the user.
+    """
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String(255), unique=True, index=True)
